8d40bc6dec974881d722aa17975b3cc8442610bc
ElenaPhoenix <sglena@mail.ru>
2022-11-28 02:03:50 +0300

    added DZ_after_Sem1\Task1

---
+++ DZ_after_Sem1/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine($"Число {numberA} является является наибольшим из введенных");
+}
+else if (numberB>numberA)
+{
+    Console.WriteLine($"Число {numberB} является является наибольшим из введенных");
+}
+else
+{
+    Console.WriteLine("Введенные числа равны!");
+}
