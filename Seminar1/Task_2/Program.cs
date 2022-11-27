274ff1f8c89a9e522f5c99cb5af09b22662a8ad2
ElenaPhoenix <sglena@mail.ru>
2022-11-27 23:22:23 +0300

    added Task_2

--- Seminar1/Task_2/Program.cs
+++ Seminar1/Task_2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое целое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+
+if (numA*numA==numB) // первый if - основное условие, если выполняется - дальше программа не смотрит
+{
+    Console.WriteLine($"Число {numB} является квадратом числа {numA}");
+}
+else if (numB*numB==numA)
+{
+    Console.WriteLine($"Число {numA} является квадратом числа {numB}");
+}
+else // в остальных случаях
+{
+    Console.WriteLine($"Квадраты чисел отсутствуют!");
+}
