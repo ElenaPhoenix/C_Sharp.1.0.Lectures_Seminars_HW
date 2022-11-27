8c298060ede229d4dc9e281672edfc47ab6f4cf5
ElenaPhoenix <sglena@mail.ru>
2022-11-28 00:57:20 +0300

    added Task_4

---
+++ Seminar1/Task_4/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int LastDigit = number%10;
+Console.WriteLine($"Последняя цифра введенного трехзначного числа - это {LastDigit}");
