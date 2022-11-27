9647da6b58fbf7dc716bf4c9a59f0a082934fecf
ElenaPhoenix <sglena@mail.ru>
2022-11-27 22:43:16 +0300

    added if, else, while to Task_1 and README.md

--- Seminar1/Task_1/Program.cs
+++ Seminar1/Task_1/Program.cs
@@ -2,3 +2,20 @@
 int num = Convert.ToInt32(Console.ReadLine());
 int square = num*num;
 Console.WriteLine("Квадрат введенного числа равен "+square);
+
+// Если хотим внутри текста вставить значение переменных:
+// Console.WriteLine($"Квадрат введенного числа равен {square}");
+
+if (num > 9) // после ветвления не ставим ;
+{
+    Console.WriteLine("Вы ввели число больше 9");
+}
+else
+{
+    Console.WriteLine("Вы ввели число меньше 10");
+}
+while (num <=25) // выводит все числа от введенного до 25
+{
+    Console.Write($"{num} ");
+    num++;
+}
