1def2f4aecf0b15561546f4a7e252019b2336a22
ElenaPhoenix <sglena@mail.ru>
2022-11-28 04:50:15 +0300

    added DZ_after_Sem1\Task2

---
+++ DZ_after_Sem1/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое целое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число, отличное от первого");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье целое число, отличное от первого и второго");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB>max) 
+{
+    max=numberB;
+    Console.WriteLine(max);
+}
+if (numberC>max)
+{
+    max=numberC; 
+    Console.WriteLine("max");
+}
+Console.WriteLine ($"Максимальное число найдено и равно {max}");
