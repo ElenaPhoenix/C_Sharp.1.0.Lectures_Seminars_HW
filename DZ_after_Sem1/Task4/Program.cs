cca8337af5941f443470e41108ef5042b9dd2b97
ElenaPhoenix <sglena@mail.ru>
2022-11-28 08:24:24 +0300

    added DZ_after_Sem1\Task4

---
+++ DZ_after_Sem1/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите целое положительное число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberA = 1;
+       
+if (numberB == 1)
+{
+    Console.WriteLine("четные числа от 1 до введенного числа отсутствуют");
+    numberB +=1;
+}
+while (numberA <= numberB);
+{
+    Console.WriteLine($"{numberA} - это все четные числа от 1 до введенного числа");
+}
