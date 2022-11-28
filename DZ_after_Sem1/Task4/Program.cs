ad09e67e85c5d8fba93d6c32480e5ca008b2fbe7
ElenaPhoenix <sglena@mail.ru>
2022-11-28 09:10:46 +0300

    changed DZ_after_Sem1\Task4

--- DZ_after_Sem1/Task4/Program.cs
+++ DZ_after_Sem1/Task4/Program.cs
@@ -2,12 +2,13 @@
 int numberB = Convert.ToInt32(Console.ReadLine());
 int numberA = 1;
        
-if (numberB == 1)
+if (numberB==1)
 {
-    Console.WriteLine("четные числа от 1 до введенного числа отсутствуют");
-    numberB +=1;
+    Console.WriteLine("положительные четные числа от 1 до введенного числа отсутствуют");
 }
-while (numberA <= numberB);
+while(numberA<numberB)
 {
-    Console.WriteLine($"{numberA} - это все четные числа от 1 до введенного числа");
+    Console.WriteLine(numberA+1);
+    numberA+=2;
 }
+    Console.WriteLine(" это все четные числа от 1 до введенного числа");
