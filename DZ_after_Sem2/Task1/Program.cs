4fa3cf398dbe6365154d7d98a94f211f8db4119d
ElenaPhoenix <sglena@mail.ru>
2022-11-29 17:01:11 +0300

    added DZ_after_Sem2\Task1

---
+++ DZ_after_Sem2/Task1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+//Console.WriteLine("Enter an integer number");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (number > 100 && number < 1000)
+//{
+//    int k = Convert.ToInt32(Math.Log10(number));
+//    int d = Convert.ToInt32(Math.Log10(k * 1000));
+//    Console.WriteLine($"The 2nd number from the left is {d}");
+//}
+//else
+//{
+//    Console.WriteLine($"There is not the 3-digits number, sorry");
+//}
+
+//Console.WriteLine("Введите трехзначное целое число");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int LastDigit = number % 10;
+//Console.WriteLine($"Последняя цифра введенного трехзначного числа - это {LastDigit}");
+
+Console.WriteLine("Enter an integer number");
+int number = Convert.ToInt32(Console.ReadLine());
+int d = 0, k = 0;
+if (number > 99 && number <1000) 
+
+{
+    k = number;
+    while (k >= 100)
+    {
+        k /= 10;
+        d = k % 10;
+    }
+    Console.WriteLine($"The 2nd number from the left is {d}");
+}
+else
+{
+    Console.WriteLine($"There is not any 3-digits numbers, sorry");
+}
--- Seminar2/Task_8.Hard/Program.cs
+++ Seminar2/Task_8.Hard/Program.cs
@@ -1,30 +1,30 @@
 ﻿// Напишите программу, которая принимает на вход числа через цикл и проверяет находятся ли там квадраты чисел каждого к каждому. И если да, выводит какое именно число является квадратом какого.
 // ДОДЕЛАТЬ
 Console.WriteLine("Введите последовательность целых чисел");
-int count= Convert.ToInt32(Console.ReadLine());
-int[] array=new int[count];
-int result=0;
+int count = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[count];
+int result = 0;
 
-for (int i=0;i<count;i++) array[i] = Convert.ToInt32 (Console.ReadLine());
+for (int i = 0; i < count; i++) array[i] = Convert.ToInt32(Console.ReadLine());
 
-for (int j=0;j<count-2;j++)
+for (int j = 0; j < count - 2; j++)
 {
-    for (int k=j+1; k<count-1;k++)
+    for (int k = j + 1; k < count - 1; k++)
     {
-        if (array[j] == array[k]*array[k])
+        if (array[j] == array[k] * array[k])
         {
             Console.WriteLine($"{array[j]} является квадратом {array[k]}");
             result++;
         }
         else
-            if (array[k] == array[j]*array[j])
-            {
-               Console.WriteLine($"{array[k]} является квадратом {array[j]}");
-               result++;
-            }
+            if (array[k] == array[j] * array[j])
+        {
+            Console.WriteLine($"{array[k]} является квадратом {array[j]}");
+            result++;
+        }
     }
 }
 if (result == 0)
 {
-    Console.WriteLine("Квадратов чисел нет"); 
+    Console.WriteLine("Квадратов чисел нет");
 }
