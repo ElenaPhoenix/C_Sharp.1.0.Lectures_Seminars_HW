468077b72adb967a99dde08748a507e0764c65d6
ElenaPhoenix <sglena@mail.ru>
2022-11-30 04:02:12 +0300

    added DZ_after_Sem2\Task2

--- DZ_after_Sem2/Task1/Program.cs
+++ DZ_after_Sem2/Task1/Program.cs
@@ -1,22 +1,4 @@
 ﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
-//Console.WriteLine("Enter an integer number");
-//int number = Convert.ToInt32(Console.ReadLine());
-//if (number > 100 && number < 1000)
-//{
-//    int k = Convert.ToInt32(Math.Log10(number));
-//    int d = Convert.ToInt32(Math.Log10(k * 1000));
-//    Console.WriteLine($"The 2nd number from the left is {d}");
-//}
-//else
-//{
-//    Console.WriteLine($"There is not the 3-digits number, sorry");
-//}
-
-//Console.WriteLine("Введите трехзначное целое число");
-//int number = Convert.ToInt32(Console.ReadLine());
-//int LastDigit = number % 10;
-//Console.WriteLine($"Последняя цифра введенного трехзначного числа - это {LastDigit}");
-
 Console.WriteLine("Enter an integer number");
 int number = Convert.ToInt32(Console.ReadLine());
 int d = 0, k = 0;
---
+++ DZ_after_Sem2/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.Через строку решать нельзя.
+Console.WriteLine("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int ind = (int)Math.Log10(num)-2;
+if (ind < 0) 
+{
+    Console.WriteLine("Третьей цифры заданного числа не существует");
+}
+else
+{
+    int res= (num % (int)Math.Pow(10, ind + 1) / (int)Math.Pow(10, ind));
+    Console.WriteLine($"Третья цифра заданного числа - {res}");
+}
