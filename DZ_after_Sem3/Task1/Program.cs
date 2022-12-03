8d20d05b6138568ddc3ced3cb7d43d11801a0fcf
ElenaPhoenix <sglena@mail.ru>
2022-12-04 00:22:48 +0300

    added DZ_after_Sem3\Task1

---
+++ DZ_after_Sem3/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Через строку решать нельзя.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.WriteLine("Введите целое число, большее 10 или меньшее -10");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num=Math.Abs (num1);
+int count=(int)Math.Log10(num) + 1; // находит количество цифр в числе
+Console.WriteLine(count);
+int sum=0;
+int res=0;
+for (int ind=0;num >10 && ind<=count &&count<=1;ind++) //проверка до 10-знаковых чисел включительно, не знаю на что переключить ToInt32
+{
+    int mun=num-sum; 
+    res = mun % 10;
+    sum=sum+(res*mun);
+    break;
+}
+if (num == sum)
+{
+    Console.WriteLine($"{sum} - палиндром");
+}
+else Console.WriteLine($"{num1} - не палиндром или в числе больше 10 знаков");
+
