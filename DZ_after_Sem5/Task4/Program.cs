510270d247dda8ec8121b8ce0b25aa78a58d4c45
ElenaPhoenix <sglena@mail.ru>
2022-12-09 14:31:16 +0300

    added DZ_after_Sem5\Task4

--- DZ_after_Sem5/Task4/Program.cs
+++ DZ_after_Sem5/Task4/Program.cs
@@ -1,51 +1,93 @@
 ﻿// Задайте массив случайных целых чисел. Найдите максимальный элемент и его индекс, минимальный элемент и его индекс, среднее арифметическое всех элементов. 
 // Сохранить эту инфу в отдельный массив и вывести на экран с пояснениями. Найти медианное значение первоначалального массива , возможно придется кое-что для этого дополнительно выполнить.
-int[] CreateArrayOfRandomNumbers(int size)  // принимает размер массива и возвращает сгенерированный массив случайных вещественных чисел
+double[] CreateArrayOfRandomNumbers(int size)  // принимает размер массива и возвращает сгенерированный массив случайных вещественных чисел
 {
-    int[] array = new int[size];
+    double[] array = new double[size];
     Random rand = new Random();
     for (int i = 0; i < size; i++)
     {
-        int num = rand.Next(1000);
+        int num = rand.Next(10);
         array[i] = num;
         Console.Write(array[i] + " ");
     }
     Console.WriteLine();
     return array;
 }
-int[] FindMaxMinAverage(int[] array) // поиск максимального и минимального элементов массива
+
+double[] FindMaxMinAverage(double[] array) // поиск максимального и минимального элементов массива с индексами, среднего арифметического
 {
-    int min = 0;
-    int max = 0;
+    int i = 0;
+    double min = array[i];
+    double max = array[i];
     int iMax = 0;
     int iMin = 0;
+    double count = 0;
+    double average = 0;
 
-    for (int i = 0; i < array.Length; i++)
+    while (i < array.Length)
     {
-                if (array[i] < max)
+        if (array[i] < min)
         {
             min = array[i];
             iMin = i;
-            Console.WriteLine($"min = {min}");
-            Console.WriteLine($"iMin = {iMin}");
         }
         if (array[i] > max)
         {
             max = array[i];
             iMax = i;
-            Console.WriteLine($"max = {max}");
-            Console.WriteLine($"iMax = {iMax}");
         }
+        count = count + array[i];
+        average = count / array.Length;
 
+        i++;
     }
-    Console.WriteLine("Максимальный элемент: " + iMax + " = " + max);
-    Console.WriteLine("Минимальный элемент: " + iMin + " = " + min);
-
-    int[] MMA = new int[] { max, min };
+    Console.WriteLine("Максимальный элемент с индексом " + iMax + " = " + max);
+    Console.WriteLine("Минимальный элемент с индексом " + iMin + " = " + min);
+    Console.WriteLine("Среднее арифметическое = " + average);
+    double[] MMA = new double[] { max, min, average };
     return MMA;
 }
+
+double[] Explanations(double[] array) // вывод найденных значений в отдельный массив с пояснениями
+{
+    int i = 0;
+    while (i <= array.Length)
+    {
+        if (i == 0) Console.WriteLine("Максимальный элемент основного массива = " + array[i]);
+        if (i == 1) Console.WriteLine("Минимальный элемент основного массива = " + array[i]);
+        if (i == 2) Console.WriteLine("Среднее арифметическое основного массива = " + array[i]);
+        i++;
+    }
+    return array;
+}
+
+double MedianOfArray(double[] array) // поиск медианы массива
+{
+
+    Array.Sort(array);
+
+    double medianValue = 0;
+
+    if (array.Length % 2 == 0) // если количество элементов четное, нужно получить два средних элемента, сложить их вместе, затем разделить на 2
+    {
+        double middleElement1 = array[(array.Length / 2) - 1];
+        double middleElement2 = array[(array.Length / 2)];
+        medianValue = (middleElement1 + middleElement2) / 2;
+        Console.WriteLine("Медианное значение основного массива = " + medianValue);
+    }
+    else // если количество элементов четное, выводим средний элемент 
+    {
+        medianValue = array[(array.Length / 2)];
+        Console.WriteLine("Медианное значение основного массива = " + medianValue);
+    }
+
+    return medianValue;
+}
+
 Console.WriteLine("Задайте размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 
-int[] Randomizer = CreateArrayOfRandomNumbers(size);
-int[] FindMMA = FindMaxMinAverage(Randomizer);
+double[] Randomizer = CreateArrayOfRandomNumbers(size);
+double[] FindMMA = FindMaxMinAverage(Randomizer);
+double[] Exp = Explanations(FindMMA);
+double Median=MedianOfArray(Randomizer);
